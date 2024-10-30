78a38f866bf094c17ab13fb2300665e9a2b16e33
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-30 23:56:51 +0200

    Update test_helpers.go

--- baseapp/test_helpers.go
+++ baseapp/test_helpers.go
@@ -1,55 +1,72 @@
 package baseapp
 
 import (
-	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	tmproto "github.com/baron-chain/cometbft-bc/proto/tendermint/types"
+	sdk "github.com/baron-chain/cosmos-bc-47/types"
+	sdkerrors "github.com/baron-chain/cosmos-bc-47/types/errors"
 )
 
-// SimCheck defines a CheckTx helper function that used in tests and simulations.
-func (app *BaseApp) SimCheck(txEncoder sdk.TxEncoder, tx sdk.Tx) (sdk.GasInfo, *sdk.Result, error) {
-	// runTx expects tx bytes as argument, so we encode the tx argument into
-	// bytes. Note that runTx will actually decode those bytes again. But since
-	// this helper is only used in tests/simulation, it's fine.
-	bz, err := txEncoder(tx)
+// SimCheck performs a CheckTx simulation and returns gas info and result.
+// It's used primarily in tests and simulations.
+func (app *BaseApp) SimCheck(txEncoder sdk.TxEncoder, tx sdk.Tx) (gasInfo sdk.GasInfo, result *sdk.Result, err error) {
+	txBytes, err := encodeTx(txEncoder, tx)
 	if err != nil {
-		return sdk.GasInfo{}, nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "%s", err)
+		return sdk.GasInfo{}, nil, err
 	}
-	gasInfo, result, _, _, err := app.runTx(runTxModeCheck, bz)
-	return gasInfo, result, err
+	
+	return app.runTxSimulation(runTxModeCheck, txBytes)
 }
 
-// Simulate executes a tx in simulate mode to get result and gas info.
-func (app *BaseApp) Simulate(txBytes []byte) (sdk.GasInfo, *sdk.Result, error) {
-	gasInfo, result, _, _, err := app.runTx(runTxModeSimulate, txBytes)
-	return gasInfo, result, err
+// Simulate executes a transaction in simulation mode to estimate gas usage.
+func (app *BaseApp) Simulate(txBytes []byte) (gasInfo sdk.GasInfo, result *sdk.Result, err error) {
+	return app.runTxSimulation(runTxModeSimulate, txBytes)
 }
 
-func (app *BaseApp) SimDeliver(txEncoder sdk.TxEncoder, tx sdk.Tx) (sdk.GasInfo, *sdk.Result, error) {
-	// See comment for Check().
-	bz, err := txEncoder(tx)
+// SimDeliver simulates a transaction delivery and returns execution results.
+func (app *BaseApp) SimDeliver(txEncoder sdk.TxEncoder, tx sdk.Tx) (gasInfo sdk.GasInfo, result *sdk.Result, err error) {
+	txBytes, err := encodeTx(txEncoder, tx)
 	if err != nil {
-		return sdk.GasInfo{}, nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "%s", err)
+		return sdk.GasInfo{}, nil, err
 	}
-	gasInfo, result, _, _, err := app.runTx(runTxModeDeliver, bz)
-	return gasInfo, result, err
+
+	return app.runTxSimulation(runTxModeDeliver, txBytes)
 }
 
-// Context with current {check, deliver}State of the app used by tests.
+// NewContext creates a new context for transaction processing.
 func (app *BaseApp) NewContext(isCheckTx bool, header tmproto.Header) sdk.Context {
 	if isCheckTx {
 		return sdk.NewContext(app.checkState.ms, header, true, app.logger).
 			WithMinGasPrices(app.minGasPrices)
 	}
-
+	
 	return sdk.NewContext(app.deliverState.ms, header, false, app.logger)
 }
 
+// NewUncachedContext creates a new context without caching.
 func (app *BaseApp) NewUncachedContext(isCheckTx bool, header tmproto.Header) sdk.Context {
 	return sdk.NewContext(app.cms, header, isCheckTx, app.logger)
 }
 
+// GetContextForDeliverTx returns the context for transaction delivery.
 func (app *BaseApp) GetContextForDeliverTx(txBytes []byte) sdk.Context {
 	return app.getContextForTx(runTxModeDeliver, txBytes)
+}
+
+// Helper functions
+
+// encodeTx encodes a transaction using the provided encoder.
+func encodeTx(txEncoder sdk.TxEncoder, tx sdk.Tx) ([]byte, error) {
+	txBytes, err := txEncoder(tx)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "failed to encode tx: %v", err)
+	}
+	return txBytes, nil
+}
+
+// runTxSimulation executes a transaction in the specified mode and returns relevant info.
+func (app *BaseApp) runTxSimulation(mode runTxMode, txBytes []byte) (sdk.GasInfo, *sdk.Result, error) {
+	gasInfo, result, _, _, err := app.runTx(mode, txBytes)
+	return gasInfo, result, err
 }
