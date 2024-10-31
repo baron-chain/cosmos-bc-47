554811b5cedd9c5b85b3538d399d46bb32c123ea
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-31 22:59:50 +0200

    Update check_type.go

--- depinject/check_type.go
+++ depinject/check_type.go
@@ -9,61 +9,89 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-// isExportedType checks if the type is exported and not in an internal
-// package. NOTE: generic type parameters are not checked because this
-// would involve complex parsing of type names (there is no reflect API for
-// generic type parameters). Parsing of these parameters should be possible
-// if someone chooses to do it in the future, but care should be taken to
-// be exhaustive and cover all cases like pointers, map's, chan's, etc. which
-// means you actually need a real parser and not just a regex.
+// ErrTypeNotExported indicates that a type is not exported (doesn't start with a capital letter)
+var ErrTypeNotExported = errors.New("type must be exported")
+
+// ErrInternalPackage indicates that a type comes from an internal package
+var ErrInternalPackage = errors.New("type must not come from an internal package")
+
+// isExportedType verifies that a type is exported and not from an internal package.
+// For composite types, it recursively checks their element types.
+// Note: Generic type parameters are not checked due to reflection limitations.
 func isExportedType(typ reflect.Type) error {
-	name := typ.Name()
-	pkgPath := typ.PkgPath()
-	if name != "" && pkgPath != "" {
-		if unicode.IsLower([]rune(name)[0]) {
-			return errors.Errorf("type must be exported: %s", typ)
-		}
-
-		pkgParts := strings.Split(pkgPath, "/")
-		if slices.Contains(pkgParts, "internal") {
-			return errors.Errorf("type must not come from an internal package: %s", typ)
-		}
-
-		return nil
+	// Check named types first
+	if isNamedType(typ) {
+		return checkNamedType(typ)
 	}
 
+	// Handle composite types
+	return checkCompositeType(typ)
+}
+
+// isNamedType returns true if the type has both a name and package path
+func isNamedType(typ reflect.Type) bool {
+	return typ.Name() != "" && typ.PkgPath() != ""
+}
+
+// checkNamedType validates that a named type is exported and not from an internal package
+func checkNamedType(typ reflect.Type) error {
+	if !isExported(typ.Name()) {
+		return errors.Wrapf(ErrTypeNotExported, "%s", typ)
+	}
+
+	if isInternalPackage(typ.PkgPath()) {
+		return errors.Wrapf(ErrInternalPackage, "%s", typ)
+	}
+
+	return nil
+}
+
+// checkCompositeType handles validation for composite types like slices, maps, etc.
+func checkCompositeType(typ reflect.Type) error {
 	switch typ.Kind() {
 	case reflect.Array, reflect.Slice, reflect.Chan, reflect.Pointer:
 		return isExportedType(typ.Elem())
 
-	case reflect.Func:
-		numIn := typ.NumIn()
-		for i := 0; i < numIn; i++ {
-			err := isExportedType(typ.In(i))
-			if err != nil {
-				return err
-			}
-		}
-
-		numOut := typ.NumOut()
-		for i := 0; i < numOut; i++ {
-			err := isExportedType(typ.Out(i))
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-
 	case reflect.Map:
-		err := isExportedType(typ.Key())
-		if err != nil {
+		if err := isExportedType(typ.Key()); err != nil {
 			return err
 		}
 		return isExportedType(typ.Elem())
 
+	case reflect.Func:
+		return checkFuncType(typ)
+
 	default:
-		// all the remaining types are builtin, non-composite types (like integers), so they are fine to use
+		// Built-in, non-composite types (integers, etc.) are always valid
 		return nil
 	}
+}
+
+// checkFuncType validates all input and output types of a function
+func checkFuncType(typ reflect.Type) error {
+	// Check input parameters
+	for i := 0; i < typ.NumIn(); i++ {
+		if err := isExportedType(typ.In(i)); err != nil {
+			return err
+		}
+	}
+
+	// Check output parameters
+	for i := 0; i < typ.NumOut(); i++ {
+		if err := isExportedType(typ.Out(i)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// isExported checks if the first character of a name is uppercase
+func isExported(name string) bool {
+	return !unicode.IsLower([]rune(name)[0])
+}
+
+// isInternalPackage checks if the package path contains an "internal" directory
+func isInternalPackage(pkgPath string) bool {
+	return slices.Contains(strings.Split(pkgPath, "/"), "internal")
 }
