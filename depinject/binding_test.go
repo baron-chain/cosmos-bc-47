08f2c499db198283cfa929e51a399967a01e33d7
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-10-31 22:58:51 +0200

    Update binding_test.go

--- depinject/binding_test.go
+++ depinject/binding_test.go
@@ -5,21 +5,17 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
+	"cosmossdk.io/depinject"
 	"github.com/regen-network/gocuke"
 	"github.com/stretchr/testify/assert"
-
-	"cosmossdk.io/depinject"
+	"github.com/stretchr/testify/require"
 )
 
-func TestBindInterface(t *testing.T) {
-	gocuke.NewRunner(t, &bindingSuite{}).
-		Path("features/bindings.feature").
-		Step(`we try to resolve a "Duck" in global scope`, (*bindingSuite).WeTryToResolveADuckInGlobalScope).
-		Step(`module "(\w+)" wants a "Duck"`, (*bindingSuite).ModuleWantsADuck).
-		Run()
-}
+const (
+	pkgPath = "cosmossdk.io/depinject_test/depinject_test"
+)
+
+// Test Types
 
 type Duck interface {
 	quack()
@@ -31,67 +27,57 @@ type (
 	Marbled    struct{}
 )
 
-func (duck Mallard) quack()    {}
-func (duck Canvasback) quack() {}
-func (duck Marbled) quack()    {}
+func (Mallard) quack()    {}
+func (Canvasback) quack() {}
+func (Marbled) quack()    {}
 
 type DuckWrapper struct {
 	Module string
 	Duck   Duck
 }
 
-func (d DuckWrapper) IsManyPerContainerType() {}
+func (DuckWrapper) IsManyPerContainerType() {}
 
 type Pond struct {
 	Ducks []DuckWrapper
 }
 
-type bindingSuite struct {
-	gocuke.TestingT // this gets injected by gocuke
+// Test Suite
 
+type bindingSuite struct {
+	gocuke.TestingT
 	configs []depinject.Config
 	pond    *Pond
 	err     error
 }
 
-func (s bindingSuite) AnInterfaceDuck() {
-	// we don't need to do anything because this is defined at the type level
+func TestBindInterface(t *testing.T) {
+	gocuke.NewRunner(t, &bindingSuite{}).
+		Path("features/bindings.feature").
+		Step(`we try to resolve a "Duck" in global scope`, (*bindingSuite).WeTryToResolveADuckInGlobalScope).
+		Step(`module "(\w+)" wants a "Duck"`, (*bindingSuite).ModuleWantsADuck).
+		Run()
 }
 
-func (s bindingSuite) TwoImplementationsMallardAndCanvasback() {
-	// we don't need to do anything because this is defined at the type level
-}
+// Provider Functions
 
 func ProvideMallard() Mallard       { return Mallard{} }
 func ProvideCanvasback() Canvasback { return Canvasback{} }
 func ProvideMarbled() Marbled       { return Marbled{} }
 
-func (s *bindingSuite) IsProvided(a string) {
-	switch a {
-	case "Mallard":
-		s.addConfig(depinject.Provide(ProvideMallard))
-	case "Canvasback":
-		s.addConfig(depinject.Provide(ProvideCanvasback))
-	case "Marbled":
-		s.addConfig(depinject.Provide(ProvideMarbled))
-	default:
-		s.Fatalf("unexpected duck type %s", a)
-	}
-}
-
-func (s *bindingSuite) addConfig(config depinject.Config) {
-	s.configs = append(s.configs, config)
-}
-
 func ProvideDuckWrapper(duck Duck) DuckWrapper {
 	return DuckWrapper{Module: "", Duck: duck}
 }
 
-func (s *bindingSuite) WeTryToResolveADuckInGlobalScope() {
-	s.addConfig(depinject.Provide(ProvideDuckWrapper))
+func ProvideModuleDuck(duck Duck, key depinject.OwnModuleKey) DuckWrapper {
+	return DuckWrapper{Module: depinject.ModuleKey(key).Name(), Duck: duck}
 }
 
-func ResolvePond(ducks []DuckWrapper) Pond { return Pond{Ducks: ducks} }
+func ResolvePond(ducks []DuckWrapper) Pond {
+	return Pond{Ducks: ducks}
+}
+
+// Suite Methods
 
 func (s *bindingSuite) resolvePond() *Pond {
 	if s.pond != nil {
@@ -105,6 +91,53 @@ func (s *bindingSuite) resolvePond() *Pond {
 	return s.pond
 }
 
+func (s *bindingSuite) addConfig(config depinject.Config) {
+	s.configs = append(s.configs, config)
+}
+
+func (s *bindingSuite) WeTryToResolveADuckInGlobalScope() {
+	s.addConfig(depinject.Provide(ProvideDuckWrapper))
+}
+
+// Duck Type Providers
+
+func (s *bindingSuite) IsProvided(duckType string) {
+	providers := map[string]func(){
+		"Mallard":    func() { s.addConfig(depinject.Provide(ProvideMallard)) },
+		"Canvasback": func() { s.addConfig(depinject.Provide(ProvideCanvasback)) },
+		"Marbled":    func() { s.addConfig(depinject.Provide(ProvideMarbled)) },
+	}
+
+	if provider, ok := providers[duckType]; ok {
+		provider()
+	} else {
+		s.Fatalf("unexpected duck type %s", duckType)
+	}
+}
+
+// Binding Methods
+
+func (s *bindingSuite) ThereIsAGlobalBindingForA(preferredType, interfaceType string) {
+	s.addConfig(depinject.BindInterface(
+		getFullTypeName(interfaceType),
+		getFullTypeName(preferredType),
+	))
+}
+
+func (s *bindingSuite) ThereIsABindingForAInModule(preferredType, interfaceType, moduleName string) {
+	s.addConfig(depinject.BindInterfaceInModule(
+		moduleName,
+		getFullTypeName(interfaceType),
+		getFullTypeName(preferredType),
+	))
+}
+
+func (s *bindingSuite) ModuleWantsADuck(module string) {
+	s.addConfig(depinject.ProvideInModule(module, ProvideModuleDuck))
+}
+
+// Verification Methods
+
 func (s *bindingSuite) IsResolvedInGlobalScope(typeName string) {
 	pond := s.resolvePond()
 	found := false
@@ -117,6 +150,20 @@ func (s *bindingSuite) IsResolvedInGlobalScope(typeName string) {
 	assert.True(s, found)
 }
 
+func (s *bindingSuite) ModuleResolvesA(module, duckType string) {
+	pond := s.resolvePond()
+	moduleFound := false
+	for _, dw := range pond.Ducks {
+		if dw.Module == module {
+			assert.Contains(s, reflect.TypeOf(dw.Duck).Name(), duckType)
+			moduleFound = true
+		}
+	}
+	assert.True(s, moduleFound)
+}
+
+// Error Handling Methods
+
 func (s *bindingSuite) ThereIsAError(expectedErrorMsg string) {
 	s.resolvePond()
 	assert.ErrorContains(s, s.err, expectedErrorMsg)
@@ -127,34 +174,13 @@ func (s *bindingSuite) ThereIsNoError() {
 	assert.NoError(s, s.err)
 }
 
-func fullTypeName(typeName string) string {
-	return fmt.Sprintf("cosmossdk.io/depinject_test/depinject_test.%s", typeName)
-}
+// No-op Methods (defined at type level)
 
-func (s *bindingSuite) ThereIsAGlobalBindingForA(preferredType string, interfaceType string) {
-	s.addConfig(depinject.BindInterface(fullTypeName(interfaceType), fullTypeName(preferredType)))
-}
+func (s bindingSuite) AnInterfaceDuck()                      {}
+func (s bindingSuite) TwoImplementationsMallardAndCanvasback() {}
 
-func (s *bindingSuite) ThereIsABindingForAInModule(preferredType string, interfaceType string, moduleName string) {
-	s.addConfig(depinject.BindInterfaceInModule(moduleName, fullTypeName(interfaceType), fullTypeName(preferredType)))
-}
+// Helper Functions
 
-func ProvideModuleDuck(duck Duck, key depinject.OwnModuleKey) DuckWrapper {
-	return DuckWrapper{Module: depinject.ModuleKey(key).Name(), Duck: duck}
-}
-
-func (s *bindingSuite) ModuleWantsADuck(module string) {
-	s.addConfig(depinject.ProvideInModule(module, ProvideModuleDuck))
-}
-
-func (s *bindingSuite) ModuleResolvesA(module string, duckType string) {
-	pond := s.resolvePond()
-	moduleFound := false
-	for _, dw := range pond.Ducks {
-		if dw.Module == module {
-			assert.Contains(s, reflect.TypeOf(dw.Duck).Name(), duckType)
-			moduleFound = true
-		}
-	}
-	assert.True(s, moduleFound)
+func getFullTypeName(typeName string) string {
+	return fmt.Sprintf("%s.%s", pkgPath, typeName)
 }
