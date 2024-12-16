8df4eed4e08e7dab8210ab38fb506afcfc896f85
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-16 23:35:14 +0200

    Update show.go

--- client/keys/show.go
+++ client/keys/show.go
@@ -1,211 +1,154 @@
 package keys
 
 import (
-	"errors"
-	"fmt"
+    "errors"
+    "fmt"
 
-	"github.com/cometbft/cometbft/libs/cli"
-	"github.com/spf13/cobra"
+    "github.com/baron-chain/cometbft-bc/libs/cli"
+    "github.com/spf13/cobra"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
-	"github.com/cosmos/cosmos-sdk/crypto/ledger"
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerr "github.com/cosmos/cosmos-sdk/types/errors"
+    "github.com/baron-chain/cosmos-sdk/client"
+    "github.com/baron-chain/cosmos-sdk/crypto/keyring"
+    "github.com/baron-chain/cosmos-sdk/crypto/keys/multisig"
+    "github.com/baron-chain/cosmos-sdk/crypto/keys/kyber"
+    "github.com/baron-chain/cosmos-sdk/crypto/ledger"
+    cryptotypes "github.com/baron-chain/cosmos-sdk/crypto/types"
+    sdk "github.com/baron-chain/cosmos-sdk/types"
+    sdkerr "github.com/baron-chain/cosmos-sdk/types/errors"
 )
 
 const (
-	// FlagAddress is the flag for the user's address on the command line.
-	FlagAddress = "address"
-	// FlagPublicKey represents the user's public key on the command line.
-	FlagPublicKey = "pubkey"
-	// FlagBechPrefix defines a desired Bech32 prefix encoding for a key.
-	FlagBechPrefix = "bech"
-	// FlagDevice indicates that the information should be shown in the device
-	FlagDevice = "device"
-
-	flagMultiSigThreshold = "multisig-threshold"
+    FlagAddress = "address"
+    FlagPublicKey = "pubkey"
+    FlagBechPrefix = "bech"
+    FlagDevice = "device"
+    FlagQuantumSafe = "quantum-safe"
+    flagMultiSigThreshold = "multisig-threshold"
 )
 
-// ShowKeysCmd shows key information for a given key name.
 func ShowKeysCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "show [name_or_address [name_or_address...]]",
-		Short: "Retrieve key information by name or address",
-		Long: `Display keys details. If multiple names or addresses are provided,
-then an ephemeral multisig key will be created under the name "multi"
-consisting of all the keys provided by name and multisig threshold.`,
-		Args: cobra.MinimumNArgs(1),
-		RunE: runShowCmd,
-	}
-	f := cmd.Flags()
-	f.String(FlagBechPrefix, sdk.PrefixAccount, "The Bech32 prefix encoding for a key (acc|val|cons)")
-	f.BoolP(FlagAddress, "a", false, "Output the address only (overrides --output)")
-	f.BoolP(FlagPublicKey, "p", false, "Output the public key only (overrides --output)")
-	f.BoolP(FlagDevice, "d", false, "Output the address in a ledger device")
-	f.Int(flagMultiSigThreshold, 1, "K out of N required signatures")
+    cmd := &cobra.Command{
+        Use:   "show [name_or_address [name_or_address...]]",
+        Short: "Retrieve quantum-safe key information",
+        Long: `Display key details with quantum-safe encryption support. 
+Multiple names or addresses create an ephemeral multisig key under "multi".`,
+        Args: cobra.MinimumNArgs(1),
+        RunE: runShowCmd,
+    }
 
-	return cmd
+    flags := cmd.Flags()
+    flags.String(FlagBechPrefix, sdk.PrefixAccount, "Bech32 prefix encoding (acc|val|cons)")
+    flags.BoolP(FlagAddress, "a", false, "Output address only")
+    flags.BoolP(FlagPublicKey, "p", false, "Output public key only")
+    flags.BoolP(FlagDevice, "d", false, "Output address in ledger device")
+    flags.BoolP(FlagQuantumSafe, "q", true, "Use quantum-safe encryption")
+    flags.Int(flagMultiSigThreshold, 1, "K out of N required signatures")
+
+    return cmd
 }
 
-func runShowCmd(cmd *cobra.Command, args []string) (err error) {
-	k := new(keyring.Record)
-	clientCtx, err := client.GetClientQueryContext(cmd)
-	if err != nil {
-		return err
-	}
-	outputFormat := clientCtx.OutputFormat
+func runShowCmd(cmd *cobra.Command, args []string) error {
+    clientCtx, err := client.GetClientQueryContext(cmd)
+    if err != nil {
+        return err
+    }
 
-	if len(args) == 1 {
-		k, err = fetchKey(clientCtx.Keyring, args[0])
-		if err != nil {
-			return fmt.Errorf("%s is not a valid name or address: %v", args[0], err)
-		}
-	} else {
-		pks := make([]cryptotypes.PubKey, len(args))
-		for i, keyref := range args {
-			k, err := fetchKey(clientCtx.Keyring, keyref)
-			if err != nil {
-				return fmt.Errorf("%s is not a valid name or address: %v", keyref, err)
-			}
-			key, err := k.GetPubKey()
-			if err != nil {
-				return err
-			}
-			pks[i] = key
-		}
-
-		multisigThreshold, _ := cmd.Flags().GetInt(flagMultiSigThreshold)
-
-		if err := validateMultisigThreshold(multisigThreshold, len(args)); err != nil {
-			return err
-		}
-
-		multikey := multisig.NewLegacyAminoPubKey(multisigThreshold, pks)
-		k, err = keyring.NewMultiRecord(k.Name, multikey)
-		if err != nil {
-			return err
-		}
-	}
-
-	isShowAddr, _ := cmd.Flags().GetBool(FlagAddress)
-	isShowPubKey, _ := cmd.Flags().GetBool(FlagPublicKey)
-	isShowDevice, _ := cmd.Flags().GetBool(FlagDevice)
-
-	isOutputSet := false
-	tmp := cmd.Flag(cli.OutputFlag)
-	if tmp != nil {
-		isOutputSet = tmp.Changed
-	}
-
-	if isShowAddr && isShowPubKey {
-		return errors.New("cannot use both --address and --pubkey at once")
-	}
-
-	if isOutputSet && (isShowAddr || isShowPubKey) {
-		return errors.New("cannot use --output with --address or --pubkey")
-	}
-
-	bechPrefix, _ := cmd.Flags().GetString(FlagBechPrefix)
-	bechKeyOut, err := getBechKeyOut(bechPrefix)
-	if err != nil {
-		return err
-	}
-
-	if isOutputSet {
-		clientCtx.OutputFormat, _ = cmd.Flags().GetString(cli.OutputFlag)
-	}
-
-	switch {
-	case isShowAddr, isShowPubKey:
-		ko, err := bechKeyOut(k)
-		if err != nil {
-			return err
-		}
-		out := ko.Address
-		if isShowPubKey {
-			out = ko.PubKey
-		}
-
-		if _, err := fmt.Fprintln(cmd.OutOrStdout(), out); err != nil {
-			return err
-		}
-	default:
-		if err := printKeyringRecord(cmd.OutOrStdout(), k, bechKeyOut, outputFormat); err != nil {
-			return err
-		}
-	}
-
-	if isShowDevice {
-		if isShowPubKey {
-			return fmt.Errorf("the device flag (-d) can only be used for addresses not pubkeys")
-		}
-		if bechPrefix != "acc" {
-			return fmt.Errorf("the device flag (-d) can only be used for accounts")
-		}
-
-		// Override and show in the device
-		if k.GetType() != keyring.TypeLedger {
-			return fmt.Errorf("the device flag (-d) can only be used for accounts stored in devices")
-		}
-
-		ledgerItem := k.GetLedger()
-		if ledgerItem == nil {
-			return errors.New("unable to get ledger item")
-		}
-
-		pk, err := k.GetPubKey()
-		if err != nil {
-			return err
-		}
-
-		return ledger.ShowAddress(*ledgerItem.Path, pk, sdk.GetConfig().GetBech32AccountAddrPrefix())
-	}
-
-	return nil
+    isQuantumSafe, _ := cmd.Flags().GetBool(FlagQuantumSafe)
+    if isQuantumSafe {
+        return handleQuantumSafeShow(cmd, args, clientCtx)
+    }
+    return handleClassicShow(cmd, args, clientCtx)
 }
 
-func fetchKey(kb keyring.Keyring, keyref string) (*keyring.Record, error) {
-	// firstly check if the keyref is a key name of a key registered in a keyring.
-	k, err := kb.Key(keyref)
-	// if the key is not there or if we have a problem with a keyring itself then we move to a
-	// fallback: searching for key by address.
+func handleQuantumSafeShow(cmd *cobra.Command, args []string, clientCtx client.Context) error {
+    k := new(keyring.Record)
+    var err error
 
-	if err == nil || !sdkerr.IsOf(err, sdkerr.ErrIO, sdkerr.ErrKeyNotFound) {
-		return k, err
-	}
+    if len(args) == 1 {
+        k, err = fetchQuantumSafeKey(clientCtx.Keyring, args[0])
+    } else {
+        k, err = handleMultiSigQuantumSafe(cmd, args, clientCtx)
+    }
+    if err != nil {
+        return err
+    }
 
-	accAddr, err := sdk.AccAddressFromBech32(keyref)
-	if err != nil {
-		return k, err
-	}
-
-	k, err = kb.KeyByAddress(accAddr)
-	return k, sdkerr.Wrap(err, "Invalid key")
+    return displayKeyInfo(cmd, k, clientCtx.OutputFormat)
 }
 
-func validateMultisigThreshold(k, nKeys int) error {
-	if k <= 0 {
-		return fmt.Errorf("threshold must be a positive integer")
-	}
-	if nKeys < k {
-		return fmt.Errorf(
-			"threshold k of n multisignature: %d < %d", nKeys, k)
-	}
-	return nil
+func fetchQuantumSafeKey(kb keyring.Keyring, keyref string) (*keyring.Record, error) {
+    k, err := kb.Key(keyref)
+    if err == nil || !sdkerr.IsOf(err, sdkerr.ErrIO, sdkerr.ErrKeyNotFound) {
+        return k, err
+    }
+
+    accAddr, err := sdk.AccAddressFromBech32(keyref)
+    if err != nil {
+        return nil, err
+    }
+
+    k, err = kb.KeyByAddress(accAddr)
+    if err != nil {
+        return nil, sdkerr.Wrap(err, "invalid quantum-safe key")
+    }
+
+    if !isQuantumSafeKey(k) {
+        return nil, errors.New("key is not quantum-safe")
+    }
+
+    return k, nil
 }
 
-func getBechKeyOut(bechPrefix string) (bechKeyOutFn, error) {
-	switch bechPrefix {
-	case sdk.PrefixAccount:
-		return keyring.MkAccKeyOutput, nil
-	case sdk.PrefixValidator:
-		return keyring.MkValKeyOutput, nil
-	case sdk.PrefixConsensus:
-		return keyring.MkConsKeyOutput, nil
-	}
+func handleMultiSigQuantumSafe(cmd *cobra.Command, args []string, clientCtx client.Context) (*keyring.Record, error) {
+    threshold, _ := cmd.Flags().GetInt(flagMultiSigThreshold)
+    if err := validateMultisigThreshold(threshold, len(args)); err != nil {
+        return nil, err
+    }
 
-	return nil, fmt.Errorf("invalid Bech32 prefix encoding provided: %s", bechPrefix)
+    pks := make([]cryptotypes.PubKey, len(args))
+    for i, keyref := range args {
+        k, err := fetchQuantumSafeKey(clientCtx.Keyring, keyref)
+        if err != nil {
+            return nil, fmt.Errorf("error processing key %s: %v", keyref, err)
+        }
+        key, err := k.GetPubKey()
+        if err != nil {
+            return nil, err
+        }
+        pks[i] = key
+    }
+
+    multikey := multisig.NewLegacyAminoPubKey(threshold, pks)
+    return keyring.NewMultiRecord("multi", multikey)
+}
+
+func isQuantumSafeKey(k *keyring.Record) bool {
+    pub, err := k.GetPubKey()
+    if err != nil {
+        return false
+    }
+    _, isKyber := pub.(*kyber.PubKey)
+    return isKyber
+}
+
+func displayKeyInfo(cmd *cobra.Command, k *keyring.Record, outputFormat string) error {
+    isShowAddr, _ := cmd.Flags().GetBool(FlagAddress)
+    isShowPubKey, _ := cmd.Flags().GetBool(FlagPublicKey)
+    isShowDevice, _ := cmd.Flags().GetBool(FlagDevice)
+
+    if isShowAddr && isShowPubKey {
+        return errors.New("cannot use both --address and --pubkey")
+    }
+
+    bechPrefix, _ := cmd.Flags().GetString(FlagBechPrefix)
+    bechKeyOut, err := getBechKeyOut(bechPrefix)
+    if err != nil {
+        return err
+    }
+
+    if isShowDevice {
+        return handleDeviceDisplay(k, bechPrefix, isShowPubKey)
+    }
+
+    return printKeyringRecord(cmd.OutOrStdout(), k, bechKeyOut, outputFormat)
 }
