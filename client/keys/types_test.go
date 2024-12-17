2ae56e6f669b241f2a04a02db0bba64402367451
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-17 23:43:39 +0200

    Update types_test.go

--- client/keys/types_test.go
+++ client/keys/types_test.go
@@ -2,28 +2,54 @@ package keys_test
 
 import (
 	"testing"
-
 	"github.com/stretchr/testify/require"
-
-	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/baron-chain/cosmos-sdk/client/keys"
+	"github.com/baron-chain/cometbft-bc/crypto/kyber"
 )
 
-func TestConstructors(t *testing.T) {
-	require.Equal(t, keys.AddNewKey{
-		Name:     "name",
-		Password: "password",
-		Mnemonic: "mnemonic",
-		Account:  1,
-		Index:    1,
-	}, keys.NewAddNewKey("name", "password", "mnemonic", 1, 1))
+func TestKeyConstructors(t *testing.T) {
+	t.Run("AddNewKey", func(t *testing.T) {
+		newKey := keys.NewAddNewKey("validator1", "quantum-safe-pwd", "test-mnemonic", 0, 0)
+		require.Equal(t, keys.AddNewKey{
+			Name:     "validator1",
+			Password: "quantum-safe-pwd",
+			Mnemonic: "test-mnemonic",
+			Account:  0,
+			Index:    0,
+		}, newKey)
+	})
 
-	require.Equal(t, keys.RecoverKey{
-		Password: "password",
-		Mnemonic: "mnemonic",
-		Account:  1,
-		Index:    1,
-	}, keys.NewRecoverKey("password", "mnemonic", 1, 1))
+	t.Run("RecoverKey", func(t *testing.T) {
+		recoveredKey := keys.NewRecoverKey("quantum-safe-pwd", "test-mnemonic", 0, 0)
+		require.Equal(t, keys.RecoverKey{
+			Password: "quantum-safe-pwd",
+			Mnemonic: "test-mnemonic",
+			Account:  0,
+			Index:    0,
+		}, recoveredKey)
+	})
 
-	require.Equal(t, keys.UpdateKeyReq{OldPassword: "old", NewPassword: "new"}, keys.NewUpdateKeyReq("old", "new"))
-	require.Equal(t, keys.DeleteKeyReq{Password: "password"}, keys.NewDeleteKeyReq("password"))
+	t.Run("UpdateKey", func(t *testing.T) {
+		updatedKey := keys.NewUpdateKeyReq("old-pwd", "new-quantum-pwd")
+		require.Equal(t, keys.UpdateKeyReq{
+			OldPassword: "old-pwd",
+			NewPassword: "new-quantum-pwd",
+		}, updatedKey)
+	})
+
+	t.Run("DeleteKey", func(t *testing.T) {
+		deletedKey := keys.NewDeleteKeyReq("quantum-safe-pwd")
+		require.Equal(t, keys.DeleteKeyReq{
+			Password: "quantum-safe-pwd",
+		}, deletedKey)
+	})
+}
+
+func TestQuantumKeyGeneration(t *testing.T) {
+	t.Run("KyberKeyPair", func(t *testing.T) {
+		privateKey, publicKey, err := kyber.GenerateKey()
+		require.NoError(t, err)
+		require.NotNil(t, privateKey)
+		require.NotNil(t, publicKey)
+	})
 }
