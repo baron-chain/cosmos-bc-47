327108ec2025f47cab8123997d53f7a11c331239
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-17 23:51:31 +0200

    Update validators.go

--- client/rpc/validators.go
+++ client/rpc/validators.go
@@ -6,142 +6,147 @@ import (
 	"strconv"
 	"strings"
 
-	tmtypes "github.com/cometbft/cometbft/types"
 	"github.com/spf13/cobra"
+	tmtypes "github.com/baron-chain/cometbft-bc/types"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/baron-chain/cosmos-bc-47/client"
+	"github.com/baron-chain/cosmos-bc-47/client/flags"
+	cryptocodec "github.com/baron-chain/cosmos-bc-47/crypto/codec"
+	cryptotypes "github.com/baron-chain/cosmos-bc-47/crypto/types"
+	sdk "github.com/baron-chain/cosmos-bc-47/types"
+	"github.com/baron-chain/cosmos-bc-47/types/query"
 )
 
-// TODO these next two functions feel kinda hacky based on their placement
+const (
+	defaultNodeEndpoint = "tcp://localhost:26657"
+	defaultLimit       = 100
+)
 
-// ValidatorCommand returns the validator set for a given height
+type ValidatorOutput struct {
+	Address          sdk.ConsAddress    `json:"address"`
+	PubKey           cryptotypes.PubKey `json:"pub_key"`
+	ProposerPriority int64             `json:"proposer_priority"`
+	VotingPower      int64             `json:"voting_power"`
+}
+
+type ValidatorsOutput struct {
+	BlockHeight int64             `json:"block_height"`
+	Validators  []ValidatorOutput `json:"validators"`
+	Total       uint64            `json:"total"`
+}
+
+func (vo ValidatorsOutput) String() string {
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "Block Height: %d\n", vo.BlockHeight)
+	fmt.Fprintf(&b, "Total Validators: %d\n", vo.Total)
+
+	for _, val := range vo.Validators {
+		fmt.Fprintf(&b, "\nValidator Details:\n")
+		fmt.Fprintf(&b, "  Address:           %s\n", val.Address)
+		fmt.Fprintf(&b, "  Public Key:        %s\n", val.PubKey)
+		fmt.Fprintf(&b, "  Proposer Priority: %d\n", val.ProposerPriority)
+		fmt.Fprintf(&b, "  Voting Power:      %d\n", val.VotingPower)
+	}
+
+	return b.String()
+}
+
 func ValidatorCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "tendermint-validator-set [height]",
-		Short: "Get the full tendermint validator set at given height",
-		Args:  cobra.MaximumNArgs(1),
+		Use:     "validator-set [height]",
+		Short:   "Get Baron Chain validator set at a given height",
+		Example: "$ barond query validator-set 1000",
+		Args:    cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to get query context: %w", err)
 			}
 
-			var height *int64
-
-			// optional height
-			if len(args) > 0 {
-				val, err := strconv.ParseInt(args[0], 10, 64)
-				if err != nil {
-					return err
-				}
-
-				if val > 0 {
-					height = &val
-				}
+			height, err := parseOptionalHeight(args)
+			if err != nil {
+				return err
 			}
 
 			page, _ := cmd.Flags().GetInt(flags.FlagPage)
 			limit, _ := cmd.Flags().GetInt(flags.FlagLimit)
 
-			result, err := GetValidators(cmd.Context(), clientCtx, height, &page, &limit)
+			result, err := QueryValidators(cmd.Context(), clientCtx, height, &page, &limit)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to query validators: %w", err)
 			}
 
 			return clientCtx.PrintObjectLegacy(result)
 		},
 	}
 
-	cmd.Flags().String(flags.FlagNode, "tcp://localhost:26657", "<host>:<port> to Tendermint RPC interface for this chain")
+	cmd.Flags().String(flags.FlagNode, defaultNodeEndpoint, "Baron Chain node RPC endpoint")
 	cmd.Flags().StringP(flags.FlagOutput, "o", "text", "Output format (text|json)")
-	cmd.Flags().Int(flags.FlagPage, query.DefaultPage, "Query a specific page of paginated results")
-	cmd.Flags().Int(flags.FlagLimit, 100, "Query number of results returned per page")
+	cmd.Flags().Int(flags.FlagPage, query.DefaultPage, "Page number for paginated results")
+	cmd.Flags().Int(flags.FlagLimit, defaultLimit, "Number of results per page")
 
 	return cmd
 }
 
-// Validator output
-type ValidatorOutput struct {
-	Address          sdk.ConsAddress    `json:"address"`
-	PubKey           cryptotypes.PubKey `json:"pub_key"`
-	ProposerPriority int64              `json:"proposer_priority"`
-	VotingPower      int64              `json:"voting_power"`
-}
-
-// Validators at a certain height output in bech32 format
-type ResultValidatorsOutput struct {
-	BlockHeight int64             `json:"block_height"`
-	Validators  []ValidatorOutput `json:"validators"`
-	Total       uint64            `json:"total"`
-}
-
-func (rvo ResultValidatorsOutput) String() string {
-	var b strings.Builder
-
-	fmt.Fprintf(&b, "block height: %d\n", rvo.BlockHeight)
-	fmt.Fprintf(&b, "total count: %d\n", rvo.Total)
-
-	for _, val := range rvo.Validators {
-		fmt.Fprintf(&b, `
-  Address:          %s
-  Pubkey:           %s
-  ProposerPriority: %d
-  VotingPower:      %d
-		`,
-			val.Address, val.PubKey, val.ProposerPriority, val.VotingPower,
-		)
+func parseOptionalHeight(args []string) (*int64, error) {
+	if len(args) == 0 {
+		return nil, nil
 	}
 
-	return b.String()
+	height, err := strconv.ParseInt(args[0], 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid height '%s': %w", args[0], err)
+	}
+
+	if height <= 0 {
+		return nil, fmt.Errorf("height must be greater than 0, got %d", height)
+	}
+
+	return &height, nil
 }
 
-func validatorOutput(validator *tmtypes.Validator) (ValidatorOutput, error) {
-	pk, err := cryptocodec.FromTmPubKeyInterface(validator.PubKey)
+func convertValidatorOutput(validator *tmtypes.Validator) (ValidatorOutput, error) {
+	pubKey, err := cryptocodec.FromTmPubKeyInterface(validator.PubKey)
 	if err != nil {
-		return ValidatorOutput{}, err
+		return ValidatorOutput{}, fmt.Errorf("failed to convert validator public key: %w", err)
 	}
 
 	return ValidatorOutput{
 		Address:          sdk.ConsAddress(validator.Address),
-		PubKey:           pk,
+		PubKey:           pubKey,
 		ProposerPriority: validator.ProposerPriority,
 		VotingPower:      validator.VotingPower,
 	}, nil
 }
 
-// GetValidators from client
-func GetValidators(ctx context.Context, clientCtx client.Context, height *int64, page, limit *int) (ResultValidatorsOutput, error) {
-	// get the node
+func QueryValidators(ctx context.Context, clientCtx client.Context, height *int64, page, limit *int) (ValidatorsOutput, error) {
 	node, err := clientCtx.GetNode()
 	if err != nil {
-		return ResultValidatorsOutput{}, err
+		return ValidatorsOutput{}, fmt.Errorf("failed to get node: %w", err)
 	}
 
 	validatorsRes, err := node.Validators(ctx, height, page, limit)
 	if err != nil {
-		return ResultValidatorsOutput{}, err
+		return ValidatorsOutput{}, fmt.Errorf("failed to query validators: %w", err)
 	}
 
-	total := validatorsRes.Total
-	if validatorsRes.Total < 0 {
-		total = 0
+	total := uint64(0)
+	if validatorsRes.Total > 0 {
+		total = uint64(validatorsRes.Total)
 	}
-	out := ResultValidatorsOutput{
-		BlockHeight: validatorsRes.BlockHeight,
-		Validators:  make([]ValidatorOutput, len(validatorsRes.Validators)),
-		Total:       uint64(total),
-	}
-	for i := 0; i < len(validatorsRes.Validators); i++ {
-		out.Validators[i], err = validatorOutput(validatorsRes.Validators[i])
+
+	validators := make([]ValidatorOutput, len(validatorsRes.Validators))
+	for i, validator := range validatorsRes.Validators {
+		validators[i], err = convertValidatorOutput(validator)
 		if err != nil {
-			return out, err
+			return ValidatorsOutput{}, err
 		}
 	}
 
-	return out, nil
+	return ValidatorsOutput{
+		BlockHeight: validatorsRes.BlockHeight,
+		Validators:  validators,
+		Total:       total,
+	}, nil
 }
